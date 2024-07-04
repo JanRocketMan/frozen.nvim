49f15a6f42f0d7446cebc7d0ef26211f79525a7f
Sergey <sergey.chervontsev@picsart.com>
2024-07-05 02:44:28 +0400

    Improve f/F and t/T moves experience with flash

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -42,8 +42,12 @@ return {
   {
     'folke/flash.nvim',
     event = "VeryLazy",
+    -- Add autojumps if match is single, add support of single-key motions (f/F and t/T)
+    -- Recommended branch: https://github.com/folke/flash.nvim/pull/283
     ---@type Flash.Config
-    opts = { jump = {autojump = true}},
+    opts = {
+      jump = {autojump = true}, modes = {char = {jump_labels = true, multi_line = false, jump = {autojump = true}}}
+    },
     -- stylua: ignore
   },
   -- Show pending keybinds.
