53ee8492fa3dc00712193ab47e849fccab34a142
JanRocketMan <cherv277@gmail.com>
2024-10-20 23:26:02 +0400

    Add experimental codecompanion tool

--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -1,26 +1,56 @@
 return {
-  'saghen/blink.cmp',
-  lazy = false, -- lazy loading handled internally
-  -- optional: provides snippets for the snippet source
-  dependencies = 'rafamadriz/friendly-snippets',
+  {
+    'saghen/blink.cmp',
+    lazy = false, -- lazy loading handled internally
+    -- optional: provides snippets for the snippet source
+    dependencies = 'rafamadriz/friendly-snippets',
 
-  -- use a release tag to download pre-built binaries
-  version = 'v0.*',
+    -- use a release tag to download pre-built binaries
+    version = 'v0.*',
 
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
-  opts = {
-    highlight = {
-      -- sets the fallback highlight groups to nvim-cmp's highlight groups
-      -- useful for when your theme doesn't support blink.cmp
-      use_nvim_cmp_as_default = true,
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      highlight = {
+        -- sets the fallback highlight groups to nvim-cmp's highlight groups
+        -- useful for when your theme doesn't support blink.cmp
+        use_nvim_cmp_as_default = true,
+      },
+      -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+      -- adjusts spacing to ensure icons are aligned
+      nerd_font_variant = 'normal',
+
+      -- experimental signature help support
+      trigger = { signature_help = { enabled = true } },
+    }
+  },
+  {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-telescope/telescope.nvim", -- Optional: For using slash commands
     },
-    -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-    -- adjusts spacing to ensure icons are aligned
-    nerd_font_variant = 'normal',
-
-    -- experimental signature help support
-    trigger = { signature_help = { enabled = true } },
+    config = function()
+      require("codecompanion").setup({
+        strategies = {
+          chat = { adapter = "ollama" },
+          inline = { adapter = "ollama" },
+          agent = { adapter = "ollama" },
+        },
+        adapters = {
+          ollama = function()
+            return require("codecompanion.adapters").extend("ollama", {
+              schema = {
+                model = {
+                  default = "llama3:latest",
+                },
+              },
+            })
+          end,
+        },
+      })
+    end,
   }
 }
 -- vim: ts=2 sts=2 sw=2 et
