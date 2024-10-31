3522649f965bf071ce56405c935c6b600d371094
Sergey <sergey.chervontsev@picsart.com>
2024-11-01 00:57:28 +0400

    Update theme to catpuccin

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -34,10 +34,16 @@ return {
   },
   -- Set default colorscheme
   {
-    'tomasiser/vim-code-dark',
+    'catppuccin/nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
-      vim.cmd.colorscheme 'codedark'
+      require("catppuccin").setup({
+        color_overrides = {
+            red = "#faa0a0",
+            maroon = "#ffd1dc",
+        },
+      })
+      vim.cmd.colorscheme 'catppuccin-macchiato'
       vim.cmd.hi 'Comment gui=none'
     end,
   },
