adfd504d93b56e354f598505372bf0f54311e1a7
Sergey <sergey.chervontsev@picsart.com>
2025-07-23 00:59:32 +0400

    Add autocmd to return to last edit positinon

--- lua/init.lua
+++ lua/init.lua
@@ -90,6 +90,18 @@ vim.api.nvim_create_autocmd("VimEnter", {
   end,
 })
 
+-- Return to last edit position when opening files
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = augroup,
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
 -- 2. [[ Basic Keymaps ]]
 
 -- Disable copy when we delete/change symbols from normal mode, keep last yanked when pasting
