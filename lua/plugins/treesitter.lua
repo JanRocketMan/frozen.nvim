45825b8108ea6d039dd573675f3de161622c9ca5
JanRocketMan <cherv277@gmail.com>
2025-10-18 22:48:57 +0400

    Fix treesitter installs

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,25 +1,33 @@
 return {
   -- Highlight, edit, and navigate code
   {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
+    "nvim-treesitter/nvim-treesitter",
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'vim', 'vimdoc', 'python' , 'zig'},
-      -- Autoinstall languages that are not installed
-      auto_install = true,
+      ensure_installed = {"bash", "c", "python", "markdown"},
+      auto_install = false,  -- never auto-install on file open
+      sync_install = true,   -- atomic installs to avoid half state
       highlight = {
         enable = true,
         -- Fix indentation issues
         additional_vim_regex_highlighting = true,
       },
     },
-    config = function(_, opts)
-      -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 
-      -- Prefer git instead of curl in order to improve connectivity in some environments
-      require('nvim-treesitter.install').prefer_git = true
-      ---@diagnostic disable-next-line: missing-fields
-      require('nvim-treesitter.configs').setup(opts)
+    config = function(_, opts)
+      local parser_dir = vim.fn.stdpath("data") .. "/ts-parsers"
+      vim.fn.mkdir(parser_dir, "p")
+      -- Make Neovim see parsers from that folder
+      vim.opt.runtimepath:append(parser_dir)
+
+      -- Make installs predictable & less flaky
+      local install = require("nvim-treesitter.install")
+      install.prefer_git = false                    -- use released tarballs instead of git clones
+      install.compilers = { "cc", "clang", "gcc" }  -- pick whatever exists on your box
+
+      -- Tell Treesitter to put compiled .so files in parser_dir
+      opts.parser_install_dir = parser_dir
+
+      require("nvim-treesitter.configs").setup(opts)
     end,
   },
   -- Rainbow parentheses for convenicency
